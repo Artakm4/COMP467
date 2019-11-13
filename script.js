afa551977bd261606a3d2d46ed4bd46c5764495e
Faisal Iqbal <juandavid@jdlondono.com>
2019-11-12 22:44:19 -0800

    Add circle select tool functionality. However, there are mysterious lines being drawn on the circles.

--- script.js
+++ script.js
@@ -1,46 +1,50 @@
 /*declare canvas and its context, make it change when window is resized*/
 var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+var context = canvas.getContext("2d");
+
 var mouse = false;
+var circleSelectBool = false;
 
 /*for brush*/
-ctx.lineJoin = "round";
-ctx.lineCap = "round";
+context.lineJoin = "round";
+context.lineCap = "round";
 
 /*keep track of mouse*/
-var positionX,positionY;
+var positionX, positionY;
 
 //Element Retrievers
 var brush = document.getElementById("brush"); //Retrieve Brush
 var eraser = document.getElementById("erase"); //Retrieve Eraser
+var circleSelectTool = document.getElementById("circle-tool");
 var reset = document.getElementById("reset"); // Retrieve Reset
 var savelink = document.getElementById("saveLink"); //Retrieve Save Image element
 
 //Default Color Conditions, makes color update and eraser work
 var color = document.getElementById("myColor");
 var myColor = color.value;
-ctx.strokeStyle = myColor;
+context.strokeStyle = myColor; //have stroke equal the color
 color.addEventListener("change", colorChange);
 
 //Update Color upon new color selection
 function colorChange(){
     myColor = color.value;
-    ctx.strokeStyle = myColor;
+    context.strokeStyle = myColor;
 }
 //End Color Selections
 
 //Set initial Brush conditions
 var size = document.getElementById("myRange");
 var mySize = size.value;
-ctx.lineWidth = mySize;
+context.lineWidth = mySize;
 size.addEventListener("change",sizeChange);//Event listener for size change
 
 //update size whenever selection is changed
 function sizeChange(){
     mySize = size.value;
-    ctx.lineWidth = mySize;
+    context.lineWidth = mySize;
 }
 
 //functions for brush to work
@@ -53,48 +57,69 @@ function getCoordinates(canvas, e){
 }
 
 function brushDraw(canvas, positionX, positionY){
+    circleSelectBool = false;
     if(mouse){
-        ctx.lineTo(positionX,positionY);
-        ctx.stroke();
+        context.lineTo(positionX,positionY);
+        context.stroke();
         canvas.style.cursor = "pointer";
     }
 }
 
 function brushMove(e){
+    circleSelectBool = false;
     var coordinates = getCoordinates(canvas,e);
     positionX = coordinates.x;
     positionY = coordinates.y;
     brushDraw(canvas, positionX, positionY);
-    
 }
 
 function brushDown(e){
+    circleSelectBool = false;
     mouse = true;
     var coordinates = getCoordinates(canvas,e);
     canvas.style.cursor = "pointer";
     positionX = coordinates.x;
     positionY = coordinates.y;
-    ctx.beginPath();
-    ctx.moveTo(positionX,positionY);
-    ctx.lineTo(positionX,positionY);
-    ctx.stroke();
+    context.beginPath();
+    context.moveTo(positionX,positionY);
+    context.lineTo(positionX,positionY);
+    context.stroke();
 }
 
 function brushUp(){
+    circleSelectBool = false;
     mouse = false;
     canvas.style.cursor = "default";
 }
 
+let n = 50;
+
+function createCircle(e) {
+    var coordinates = getCoordinates(canvas,e);
+    canvas.style.cursor = "pointer";
+    positionX = coordinates.x;
+    positionY = coordinates.y;
+    //context.beginPath();
+    //context.moveTo(positionX,positionY);
+    //context.lineTo(positionX,positionY);
+    //context.stroke();
+    context.arc(positionX, positionY, 50, 0, 2 * Math.PI);
+    context.stroke();
+    n += 2000;
+}
+
 function brushClick(){
+    circleSelectBool = false;
+    canvas.removeEventListener("click",createCircle); 
     //get color of brush
     var brushColor = document.getElementById("myColor");
-    ctx.strokeStyle = brushColor.value;
+    context.strokeStyle = brushColor.value;
     
     //Border style of buttons put nones before border
     eraser.style.border = "none";
+    circleSelectTool.style.border = "none";
     brush.style.border = "2px solid red";
-    
-    
+     
     canvas.addEventListener("mousedown",brushDown,false);
     canvas.addEventListener("mousemove",brushMove,false);
     canvas.addEventListener("mouseup",brushUp,false);
@@ -103,9 +128,12 @@ function brushClick(){
 
 //Start of Eraser functions
 function eraserClick(){
-    ctx.strokeStyle = "white";
+    circleSelectBool = false;
+    canvas.removeEventListener("click",createCircle); 
+    context.strokeStyle = "white";
     //Give border to button made sure to unselect all buttons in future
     brush.style.border = "none";
+    circleSelectTool.style.border = "none";
     eraser.style.border = "2px solid red";
     
     canvas.addEventListener("mousedown",brushDown,false);
@@ -113,16 +141,42 @@ function eraserClick(){
     canvas.addEventListener("mouseup",brushUp,false);
 }
 
+function circleSelect() {
+    
+    circleSelectBool = true;
+    let circleSelectActivate = true;
+    context.strokeStyle = "black";
+    //Give border to button made sure to unselect all buttons in future
+    brush.style.border = "none";
+    eraser.style.border = "none";
+
+    circleSelectTool.style.border = "2px solid red";
+
+    canvas.removeEventListener("mousedown",brushDown);
+    canvas.removeEventListener("mousemove",brushMove);
+    canvas.removeEventListener("mouseup",brushUp);
+
+    if ( (circleSelectBool === true) && (circleSelectActivate === true) ) {
+        canvas.addEventListener("click",createCircle,false); 
+    }
+    //canvas.addEventListener("mousemove",null,false);
+    //canvas.addEventListener("mouseup",null,false);
+};
+
 //End of Eraser functions
 
 // Reset Button Function
 function resetClick() {
+    circleSelectTool = false;
+    canvas.removeEventListener("click",createCircle); 
     window.location.reload();
 }
 //End Reset Button Functions
 
 //Save Image Button Function
 function saveClick() {
+    circleSelectTool = false;
+    canvas.removeEventListener("click",createCircle); 
     var data = canvas.toDataURL();
     saveLink.href = data;
     saveLink.download = "myImage.png";
@@ -134,3 +188,5 @@ brush.addEventListener("click",brushClick); //Starts brush event on click
 erase.addEventListener("click",eraserClick); //Start eraser event on click
 reset.addEventListener("click",resetClick); //Start Reset Click Event
 saveLink.addEventListener("click",saveClick); //Start Save Click Event
+circleSelectTool.addEventListener("click", circleSelect);
+ 
